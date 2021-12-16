1b7a6a594dd4edacb502398c8318c2b7828c38b3
Seyoung Lee <seyounglee@improbable.io>
2021-12-16 15:44:30 +0000

    "main.py" fallen out of use

--- main.py
+++
@@ -1,6 +0,0 @@
-from girb import GIBScraper, Summer
-
-if __name__ == "__main__":
-    sc = GIBScraper("https://www.gamesindustry.biz/articles/2021-12-15-game-changers-2021-part-six")
-    sm = Summer(sc.get_article_as_sentences_list())
-    print(sm.generate_summary())
